9e8121173f907b3dbbbfcb3930dec5cbc1a5652d
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-19 16:26:03 +0300

    React-router-dom set-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className='App'>
-      <h1>App</h1>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<h1>Home page</h1>} />
+        <Route path='/cars' element={<h1>All cars</h1>} />
+      </Routes>
     </div>
   );
 }
