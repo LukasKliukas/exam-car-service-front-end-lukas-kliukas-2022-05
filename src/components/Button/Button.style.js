bfe1a28e614961f59f61e76a54fe586565948b46
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-19 15:45:39 +0300

    Button style edited

--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+    'Lucida Sans', Arial, sans-serif;
   background: ${(props) => props.color};
   border: none;
   border-radius: 1rem;
   color: #ef4623;
-  font-style: italic;
   padding: 0.5rem 1rem;
   text-decoration: none;
   cursor: pointer;
   :hover {
     background: #fbd0c7;
-    box-shadow: 2px 2px 2px #2986cc;
+    box-shadow: 2px 2px 2px #64686c;
     color: #2c343c;
   }
 `;
