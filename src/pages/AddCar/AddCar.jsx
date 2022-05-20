a9e43da84703915ee4836d78fc79633830649ea4
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-20 15:41:27 +0300

    AddCar page created

--- src/pages/AddCar/AddCar.jsx
+++ src/pages/AddCar/AddCar.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Input from '../../components/Input/Input';
+import Wrapper from '../../components/Wrapper/Wrapper';
 
 const AddCar = (props) => {
-  return <div>AddCar</div>;
+  return (
+    <Wrapper>
+      <h1>Add a Car :</h1>
+      <Input
+        label='Add a new task'
+        name='input'
+        type='text'
+        placeholder='Text...'
+        handleChange={() => {
+          console.log('labas');
+        }}
+      />
+    </Wrapper>
+  );
 };
 
 AddCar.propTypes = {};
