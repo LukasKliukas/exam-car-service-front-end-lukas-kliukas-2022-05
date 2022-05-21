ee9244e4f38193c7b1ea6187956848e0dac5cd96
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-21 19:55:58 +0300

    Wrapper minor change

--- src/components/Wrapper/Wrapper.style.js
+++ src/components/Wrapper/Wrapper.style.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  margin: 0 auto;
-  max-width: 60rem;
+  margin: 0 1rem;
+  max-width: 120rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
