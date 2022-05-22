74fb018d91d5c1a8b1853aa4420e726d27b055c3
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-22 16:27:27 +0300

    if statements

--- src/components/CarsCard/CarsCard.jsx
+++ src/components/CarsCard/CarsCard.jsx
@@ -4,17 +4,21 @@ import * as S from './CarsCard.style';
 import Button from './../Button/Button';
 
 const CarsCard = (props) => {
-  return (
-    <S.CarWrapper>
-      <S.CarBrand>{props.brand}</S.CarBrand>
-      <S.CarModel>{props.model}</S.CarModel>
-      <S.CarYear>{props.year}</S.CarYear>
-      <S.CarNumber>{props.number}</S.CarNumber>
-      <S.CarOwner>{props.owner}</S.CarOwner>
-      <S.CarPhone>{props.phone}</S.CarPhone>
-      <Button>X</Button>
-    </S.CarWrapper>
-  );
+  if (props) {
+    return (
+      <S.CarWrapper>
+        <S.CarBrand>{props.brand}</S.CarBrand>
+        <S.CarModel>{props.model}</S.CarModel>
+        <S.CarYear>{props.year}</S.CarYear>
+        <S.CarNumber>{props.number}</S.CarNumber>
+        <S.CarOwner>{props.owner}</S.CarOwner>
+        <S.CarPhone>{props.phone}</S.CarPhone>
+        <Button>X</Button>
+      </S.CarWrapper>
+    );
+  } else {
+    return <h1>No data from database</h1>;
+  }
 };
 
 CarsCard.propTypes = {};
--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -3,23 +3,27 @@ import CarsCard from './../CarsCard/CarsCard';
 import * as S from './CarsList.style';
 
 const CarsList = ({ data }) => {
-  return (
-    <S.CarsGrid>
-      {data &&
-        data.map((car) => (
-          <CarsCard
-            key={car.id}
-            brand={car.brand}
-            model={car.model}
-            year={car.year}
-            number={car.number}
-            owner={car.owner}
-            phone={car.phone}
-          />
-        ))}
-      {!data && <h2>No cars created, please create some in add car page !</h2>}
-    </S.CarsGrid>
-  );
+  console.log(data);
+  if (data.length > 0) {
+    return (
+      <S.CarsGrid>
+        {data.length > 0 &&
+          data.map((car) => (
+            <CarsCard
+              key={car.id}
+              brand={car.brand}
+              model={car.model}
+              year={car.year}
+              number={car.number}
+              owner={car.owner}
+              phone={car.phone}
+            />
+          ))}
+      </S.CarsGrid>
+    );
+  } else {
+    return <h2>No cars created, please create some in add a car page !</h2>;
+  }
 };
 
 export default CarsList;
