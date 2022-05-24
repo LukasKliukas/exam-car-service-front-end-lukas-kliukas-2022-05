b75853fa4888fb42154b354b63c86ab76a2cab85
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-24 19:24:20 +0300

    AddCar form edited

--- src/pages/AddCar/AddCar.jsx
+++ src/pages/AddCar/AddCar.jsx
@@ -3,10 +3,14 @@ import Input from '../../components/Input/Input';
 import Wrapper from '../../components/Wrapper/Wrapper';
 import Button from '../../components/Button/Button';
 import * as S from './AddCar.style';
+import Title from '../../components/Title/Title';
+import { useNavigate } from 'react-router-dom';
 
 const token = localStorage.getItem('token');
 
 const AddCar = (props) => {
+  let navigate = useNavigate();
+
   const [brand, setBrand] = useState('');
   const [model, setModel] = useState('');
   const [year, setYear] = useState('');
@@ -14,6 +18,7 @@ const AddCar = (props) => {
   const [owner, setOwner] = useState('');
   const [phone, setPhone] = useState('');
   const [message, setMessage] = useState('');
+  const [popUpInfo, setPopUpInfo] = useState('');
 
   async function sendPostFetch() {
     const newPostObj = {
@@ -24,6 +29,10 @@ const AddCar = (props) => {
       phone: phone,
       year: year,
     };
+    if (isThereErrors(newPostObj)) {
+      alert('All fields are required !');
+      return;
+    }
     const resp = await fetch(`${process.env.REACT_APP_SERVER_URL}/cars`, {
       method: 'POST',
       headers: {
@@ -41,16 +50,33 @@ const AddCar = (props) => {
     if (atsInJs.err) {
       setMessage(atsInJs.err);
     }
+    setPopUpInfo('Car succesefully created !');
   }
-
   function submitHandler(e) {
     e.preventDefault();
     sendPostFetch();
+    // window.location.reload();
+  }
+
+  function isThereErrors(dataToCheck) {
+    const mustBeKeys = ['brand', 'model', 'number', 'owner', 'phone', 'year'];
+    const values = Object.values(dataToCheck);
+    const valuesBool = values
+      .map((val) => !!val)
+      .filter((val) => val === false);
+    const ourKeys = Object.keys(dataToCheck);
+    const allKeys = mustBeKeys.filter((mustKey) => !ourKeys.includes(mustKey));
+    if (valuesBool.length > 0 || allKeys.length > 0) {
+      console.log('Not all data written to a form');
+      return true;
+    }
+    return false;
   }
 
   return (
     <Wrapper>
-      <h1>Add a new Car :</h1>
+      <Title>Add a new Car :</Title>
+      {popUpInfo.length > 0 ? <S.InfoPopUp>{popUpInfo}</S.InfoPopUp> : ''}
       <form onSubmit={submitHandler}>
         {message && <h2>{message}</h2>}
         <Input
--- src/pages/AddCar/AddCar.style.js
+++ src/pages/AddCar/AddCar.style.js
@@ -3,3 +3,6 @@ import styled from 'styled-components';
 export const TableWrap = styled.form`
   border: 1px solid #ef4623;
 `;
+export const InfoPopUp = styled.h2`
+  color: #ef4623;
+`;
