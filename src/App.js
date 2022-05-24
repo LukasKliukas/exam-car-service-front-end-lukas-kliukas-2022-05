c7c36b2edd30e421a1d5e2a061315a5f4548a83b
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-24 19:22:30 +0300

    New routes added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import AddCar from './pages/AddCar/AddCar';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className='App'>
       <Navigation />
       <Routes>
-        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/cars' element={<Home />} />
         <Route path='/addCar' element={<AddCar />} />
       </Routes>
