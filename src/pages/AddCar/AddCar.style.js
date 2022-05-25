371e73289aeb986f45bc226f0f6f30f818a696b3
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-25 11:04:39 +0300

    Background added to a page

--- src/pages/AddCar/AddCar.style.js
+++ src/pages/AddCar/AddCar.style.js
@@ -6,3 +6,10 @@ export const TableWrap = styled.form`
 export const InfoPopUp = styled.h2`
   color: #ef4623;
 `;
+
+export const Background = styled.div`
+  background-image: url('https://i.pinimg.com/originals/dc/19/e9/dc19e9b94a372ebc21ffeb7623d5632a.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+`;
