52b21b18e1d6a466ec126cd4a44f88d92390a56c
Lukas Kliukas <lukaskliukas@gmail.com>
2022-05-25 15:47:53 +0300

    Styled components written

---
+++ src/pages/Login/Login.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+  background-image: url('https://purepng.com/public/uploads/large/purepng.com-audi-r8audicars-961524670148fabn4.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+`;
